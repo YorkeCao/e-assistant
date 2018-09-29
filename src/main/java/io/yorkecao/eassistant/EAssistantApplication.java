9c8ad077015943f54f636f81198e9318a113def0
Yorke <yorkecao@gmail.com>
2018-09-29 10:51:41 +0800

    :tada: 项目初始化

---
+++ src/main/java/io/yorkecao/eassistant/EAssistantApplication.java
@@ -0,0 +1,12 @@
+package io.yorkecao.eassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EAssistantApplication.class, args);
+    }
+}
